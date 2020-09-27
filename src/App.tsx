9d6be2c91974a1bcb3a370c10e0bfc894225aedb
Paulo Roberto <prmorais1302@gmail.com>
2020-09-27 11:45:59 -0300

    11. Criando os temas de cores da aplicacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 
 import GlobalStyles from "./styles/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+
 import Layout from "./components/Layout";
+import Dark from "./styles/themes/Dark";
 
 const App: React.FC = () => {
   return (
-    <>
+    <ThemeProvider theme={Dark}>
       <GlobalStyles />
       <Layout />
-    </>
+    </ThemeProvider>
   );
 };
 
