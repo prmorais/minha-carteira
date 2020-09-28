c4f6d63078b6a73e54163f2ca4b5f2b16de63e27
Paulo Roberto <prmorais1302@gmail.com>
2020-09-27 22:50:36 -0300

    15. Criando o header de conteudo

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,15 @@ import { ThemeProvider } from "styled-components";
 
 import Layout from "./components/Layout";
 import Dark from "./styles/themes/Dark";
+import Dashboard from "./pages/Dashboard";
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={Dark}>
       <GlobalStyles />
-      <Layout />
+       <Layout>
+          <Dashboard/>
+       </Layout>
     </ThemeProvider>
   );
 };
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 
+import {Container} from './styled'
+import ContentHeader from "../../components/ContentHeader";
+
 const Dashboard: React.FC = () => {
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+   <Container>
+      <ContentHeader />
+   </Container>
   );
 };
 
